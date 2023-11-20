31461a9537e1505c57caf3a9727a2ee13edd660d
Tim Hogarty <thogarty@equinix.com>
2023-11-20 11:08:42 -0800

    Add port module, some completed examples, and example skeletons for non-completed examples

---
+++ examples/port-2-aws-connection/outputs.tf
@@ -0,0 +1,3 @@
+output "connection_id" {
+  value = module.create_port_2_aws_connection.primary_connection_id
+}
---
+++ examples/port-2-azure-connection/outputs.tf
@@ -0,0 +1,3 @@
+output "connection_id" {
+  value = module.create_port_2_azure_connection.primary_connection_id
+}
