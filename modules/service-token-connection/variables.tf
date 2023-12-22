62087c82c4cd9c03ae0838f3b64d79b9be65e32f
srushti-patl <srpatel@equinix.com>
2023-12-21 16:38:47 -0800

    CXF-83183 Adding Service Token module and metal-2-fabric example with Metal provider

---
+++ modules/service-token-connection/variables.tf
@@ -0,0 +1,91 @@
+variable "connection_name" {
+  description = "Connection name. An alpha-numeric 24 characters string which can include only hyphens and underscores"
+  type        = string
+}
+variable "connection_type" {
+  description = "Defines the connection type like VG_VC, EVPL_VC, EPL_VC, EC_VC, IP_VC, ACCESS_EPL_VC"
+  type        = string
+}
+
+variable "notifications_type" {
+  description = "Notification Type - ALL is the only type currently supported"
+  type        = string
+  default     = "ALL"
+}
+variable "notifications_emails" {
+  description = "Array of contact emails"
+  type        = list(string)
+}
+
+variable "bandwidth" {
+  description = "Connection bandwidth in Mbps"
+  type        = number
+}
+variable "purchase_order_number" {
+  description = "Purchase order number"
+  type        = string
+  default     = ""
+}
+variable "aside_service_token_uuid" {
+  description = "Equinix A-Side Service Token UUID"
+  type        = string
+  default     = ""
+}
+variable "zside_ap_type" {
+  description = "Access point type - VD, SP, COLO, CLOUD_ROUTER, NETWORK"
+  type        = string
+}
+variable "zside_ap_authentication_key" {
+  description = "Authentication key for provider based connections"
+  type        = string
+  default     = ""
+}
+variable "zside_ap_profile_type" {
+  description = "Service profile type - L2_PROFILE, L3_PROFILE, ECIA_PROFILE, ECMC_PROFILE"
+  type        = string
+  default     = ""
+}
+variable "zside_location" {
+  description = "Access point metro code"
+  type        = string
+}
+variable "zside_sp_name" {
+  description = "Equinix Service Profile Name"
+  type        = string
+  default     = ""
+}
+variable "zside_port_name" {
+  description = "Equinix Z-Side Port Name"
+  type        = string
+  default     = ""
+}
+variable "zside_seller_region" {
+  description = "Access point seller region"
+  type        = string
+  default     = ""
+}
+variable "zside_vlan_tag" {
+  description = "VLan Tag information for DOT1Q connections, and the outer VLan tag for QINQ connections"
+  type        = string
+  default     = ""
+}
+variable "zside_vlan_inner_tag" {
+  description = "Inner VLan tag for QINQ connections"
+  type        = string
+  default     = ""
+}
+variable "zside_peering_type" {
+  description = "Zside Access Point Peering type. Available values; PRIVATE, MICROSOFT, PUBLIC, MANUAL"
+  type        = string
+  default     = ""
+}
+variable "zside_service_token_uuid" {
+  description = "Service Token UUID"
+  type        = string
+  default     = ""
+}
+variable "additional_info" {
+  description = "Additional info parameters. It's a list of maps containing 'key' and 'value' keys with their corresponding values."
+  type        = list(object({ key = string, value = string }))
+  default     = []
+}
