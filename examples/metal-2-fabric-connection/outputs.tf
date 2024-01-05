4b5e9bb791a728365c5c91e807bbb1283939eba8
srushti-patl <srpatel@equinix.com>
2024-01-05 12:45:02 -0800

    CXF-83183 removing old outputs.tf file

--- examples/metal-2-fabric-connection/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "metal-connection" {
-  value = equinix_metal_connection.metal-connection.id     // changed to equinix metal
-}
-
-output "fabric-connection" {
-  value = module.metal-2-fabric-connection.primary_connection_id
-}
