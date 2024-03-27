97f2713989ee0a29d1ef7af5c18b26f0f9d518ce
srushti-patl <srpatel@equinix.com>
2024-03-26 22:49:09 -0700

    feat: Adding metal provider examples for NIMF functionality

--- examples/NIMF-Metal-2-AWS-connection/main.tf
+++ examples/NIMF-Metal-2-AWS-connection/main.tf
@@ -1,6 +1,7 @@
 provider "equinix" {
   client_id     = var.equinix_client_id
   client_secret = var.equinix_client_secret
+  auth_token    = var.metal_auth_token
 }
 data "equinix_fabric_service_profiles" "zside_sp" {
   filter {
@@ -9,9 +10,24 @@ data "equinix_fabric_service_profiles" "zside_sp" {
     values   = [var.zside_sp_name]
   }
 }
+resource "equinix_metal_vlan" "vlan-server" {
+  description = "${var.metal_connection_metro} VLAN Server 1 to Cloud"
+  metro       = var.metal_connection_metro
+  project_id  = var.metal_project_id
+}
+resource "equinix_metal_connection" "metal-connection" {
+  name          = var.metal_connection_name
+  redundancy    = var.metal_connection_redundancy
+  speed         = var.metal_connection_speed
+  type          = var.metal_connection_type
+  project_id    = var.metal_project_id
+  metro         = var.metal_connection_metro
+  vlans         = [equinix_metal_vlan.vlan-server.vxlan]
+  contact_email = var.metal_contact_email
+}
 resource "equinix_fabric_connection" "metal-2-aws-connection" {
-  name = var.connection_name
-  type = var.connection_type
+  name      = var.connection_name
+  type      = var.connection_type
   bandwidth = var.bandwidth
   project {
     project_id = var.project_id
@@ -23,25 +39,24 @@ resource "equinix_fabric_connection" "metal-2-aws-connection" {
   order {
     purchase_order_number = var.purchase_order_number != "" ? var.purchase_order_number : null
   }
-
   additional_info = var.additional_info != [] ? var.additional_info : null
   a_side {
     access_point {
-      type = var.aside_ap_type
-      authentication_key = var.aside_ap_authentication_key
+      type               = var.aside_ap_type
+      authentication_key = equinix_metal_connection.metal-connection.authorization_code
     }
   }
   z_side {
     access_point {
-      type= var.zside_ap_type
-      authentication_key= var.zside_ap_authentication_key
-      seller_region = var.zside_seller_region
+      type                = var.zside_ap_type
+      authentication_key  = var.zside_ap_authentication_key
+      seller_region       = var.zside_seller_region
       profile {
-        type= var.zside_ap_profile_type
-        uuid= data.equinix_fabric_service_profiles.zside_sp.data.0.uuid
+        type  = var.zside_ap_profile_type
+        uuid  = data.equinix_fabric_service_profiles.zside_sp.data.0.uuid
       }
       location {
-        metro_code= var.zside_location
+        metro_code  = var.zside_location
       }
     }
   }
--- examples/NIMF-Metal-2-AWS-connection/outputs.tf
+++ examples/NIMF-Metal-2-AWS-connection/outputs.tf
@@ -1,3 +1,9 @@
-output "metal-2aws_connection_id" {
+output "metal_vlan_id" {
+  value = equinix_metal_vlan.vlan-server.id
+}
+output "metal_connection_id" {
+  value = equinix_metal_connection.metal-connection.id
+}
+output "metal_aws_connection_id" {
   value = equinix_fabric_connection.metal-2-aws-connection.id
 }
