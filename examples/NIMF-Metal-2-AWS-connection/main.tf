94ee636a78cbea7d01e21d75a6080e9b950205b8
srushti-patl <srpatel@equinix.com>
2024-03-28 10:46:25 -0700

    feat: Adding metal-connection sub-module and updating NIMF example with AWS provider

--- examples/NIMF-Metal-2-AWS-connection/main.tf
+++ examples/NIMF-Metal-2-AWS-connection/main.tf
@@ -3,12 +3,10 @@ provider "equinix" {
   client_secret = var.equinix_client_secret
   auth_token    = var.metal_auth_token
 }
-data "equinix_fabric_service_profiles" "zside_sp" {
-  filter {
-    property = "/name"
-    operator = "="
-    values   = [var.zside_sp_name]
-  }
+provider "aws" {
+  access_key = var.additional_info[0]["value"]
+  secret_key = var.additional_info[1]["value"]
+  region     = var.zside_seller_region
 }
 resource "equinix_metal_vlan" "vlan-server" {
   description = "${var.metal_connection_metro} VLAN Server 1 to Cloud"
@@ -25,39 +23,57 @@ resource "equinix_metal_connection" "metal-connection" {
   vlans         = [equinix_metal_vlan.vlan-server.vxlan]
   contact_email = var.metal_contact_email
 }
-resource "equinix_fabric_connection" "metal-2-aws-connection" {
-  name      = var.connection_name
-  type      = var.connection_type
-  bandwidth = var.bandwidth
-  project {
-    project_id = var.project_id
-  }
-  notifications {
-    emails = var.notifications_emails
-    type   = var.notifications_type
-  }
-  order {
-    purchase_order_number = var.purchase_order_number != "" ? var.purchase_order_number : null
-  }
-  additional_info = var.additional_info != [] ? var.additional_info : null
-  a_side {
-    access_point {
-      type               = var.aside_ap_type
-      authentication_key = equinix_metal_connection.metal-connection.authorization_code
-    }
-  }
-  z_side {
-    access_point {
-      type                = var.zside_ap_type
-      authentication_key  = var.zside_ap_authentication_key
-      seller_region       = var.zside_seller_region
-      profile {
-        type  = var.zside_ap_profile_type
-        uuid  = data.equinix_fabric_service_profiles.zside_sp.data.0.uuid
-      }
-      location {
-        metro_code  = var.zside_location
-      }
-    }
-  }
+
+module "metal_2_aws_connection" {
+  source = "../../modules/metal-connection"
+
+  connection_name       = var.connection_name
+  connection_type       = var.connection_type
+  notifications_type    = var.notifications_type
+  notifications_emails  = var.notifications_emails
+  project_id            = var.project_id
+  additional_info       = var.additional_info
+  bandwidth             = var.bandwidth
+  purchase_order_number = var.purchase_order_number
+
+  aside_ap_type               = var.aside_ap_type
+  aside_ap_authentication_key = equinix_metal_connection.metal-connection.authorization_code
+
+  zside_ap_type               = var.zside_ap_type
+  zside_ap_authentication_key = var.zside_ap_authentication_key
+  zside_seller_region         = var.zside_seller_region
+  zside_ap_profile_type       = var.zside_ap_profile_type
+  zside_location              = var.zside_location
+  zside_fabric_sp_name        = var.zside_fabric_sp_name
+}
+
+data "aws_dx_connection" "aws_connection" {
+  depends_on = [
+    module.metal_2_aws_connection
+  ]
+  name = var.connection_name
+}
+
+resource "aws_dx_gateway" "aws_gateway" {
+  depends_on = [
+    module.metal_2_aws_connection
+  ]
+  name            = var.aws_gateway_name
+  amazon_side_asn = var.aws_gateway_asn
+}
+
+resource "aws_dx_private_virtual_interface" "aws_virtual_interface" {
+  depends_on = [
+    module.metal_2_aws_connection,
+    aws_dx_gateway.aws_gateway
+  ]
+  connection_id    = data.aws_dx_connection.aws_connection.id
+  name             = var.aws_vif_name
+  vlan             = data.aws_dx_connection.aws_connection.vlan_id
+  address_family   = var.aws_vif_address_family
+  bgp_asn          = var.aws_vif_bgp_asn
+  amazon_address   = var.aws_vif_amazon_address
+  customer_address = var.aws_vif_customer_address
+  bgp_auth_key     = var.aws_vif_bgp_auth_key
+  dx_gateway_id    = aws_dx_gateway.aws_gateway.id
 }
--- examples/NIMF-Metal-2-AWS-connection/variables.tf
+++ examples/NIMF-Metal-2-AWS-connection/variables.tf
@@ -84,7 +84,7 @@ variable "zside_ap_profile_type" {
   description = "Service profile type - L2_PROFILE, L3_PROFILE, ECIA_PROFILE, ECMC_PROFILE"
   type        = string
 }
-variable "zside_sp_name" {
+variable "zside_fabric_sp_name" {
   description = "Equinix Service Profile Name"
   type        = string
 }
@@ -100,4 +100,40 @@ variable "additional_info" {
   description = "Additional info parameters. It's a list of maps containing 'key' and 'value' keys with their corresponding values."
   type        = list(object({ key = string, value = string }))
   default     = []
+}
+variable "aws_vif_name" {
+  description = "The name for the virtual interface"
+  type        = string
+}
+variable "aws_vif_address_family" {
+  description = "The address family for the BGP peer. ipv4 or ipv6"
+  type        = string
+}
+variable "aws_vif_bgp_asn" {
+  description = "The autonomous system (AS) number for Border Gateway Protocol (BGP) configuration"
+  type        = number
+}
+variable "aws_vif_amazon_address" {
+  description = "The IPv4 CIDR address to use to send traffic to Amazon. Required for IPv4 BGP peers"
+  type        = string
+  default     = ""
+}
+variable "aws_vif_customer_address" {
+  description = "The IPv4 CIDR destination address to which Amazon should send traffic. Required for IPv4 BGP peers"
+  type        = string
+  default     = ""
+}
+variable "aws_vif_bgp_auth_key" {
+  description = "The authentication key for BGP configuration"
+  type        =  string
+  default     = ""
+  sensitive   = true
+}
+variable "aws_gateway_name" {
+  description = "The name of the Gateway"
+  type        = string
+}
+variable "aws_gateway_asn" {
+  description = "The ASN to be configured on the Amazon side of the connection. The ASN must be in the private range of 64,512 to 65,534 or 4,200,000,000 to 4,294,967,294"
+  type        = number
 }
