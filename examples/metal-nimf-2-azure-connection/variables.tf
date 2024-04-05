87324f59c6ee330a1654c2fc9e1a6e63c239a7c2
srushti-patl <srpatel@equinix.com>
2024-04-05 14:03:52 -0700

    feat:Marking azure credentials variables as sensitive

--- examples/metal-nimf-2-azure-connection/variables.tf
+++ examples/metal-nimf-2-azure-connection/variables.tf
@@ -94,18 +94,22 @@ variable "zside_location" {
 variable "azure_client_id" {
   description = "Azure Client id"
   type        = string
+  sensitive   = true
 }
 variable "azure_client_secret" {
   description = "Azure Secret value"
   type        = string
+  sensitive   = true
 }
 variable "azure_tenant_id" {
   description = "Azure Tenant id"
   type        = string
+  sensitive   = true
 }
 variable "azure_subscription_id" {
   description = "Azure Subscription id"
   type        = string
+  sensitive   = true
 }
 variable "azure_resource_name" {
   description = "The name of Azure Resource"
