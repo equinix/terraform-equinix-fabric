0ba4d537745abac05b9db7cf1c28501b6a30b059
jkallem <jkallem@equinix.com>
2024-06-07 14:56:41 -0700

    Added Name Update, Bandwidth Upgrade & Bandwidth Downgrade

--- tests/prod/prod_sanity_suite_test.go
+++ tests/prod/prod_sanity_suite_test.go
@@ -6,47 +6,56 @@ import (
 	"testing"
 )
 
-// func TestPort2AlibabaCreateConnection_DIGP(t *testing.T) {
-//
-//		terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-//			TerraformDir: "../../examples/port-2-alibaba-connection",
-//		})
-//
-//		defer terraform.Destroy(t, terraformOptions)
-//		t.Parallel()
-//
-//		terraform.InitAndApply(t, terraformOptions)
-//		output := terraform.Output(t, terraformOptions, "alibaba_connection_id")
-//		assert.NotNil(t, output)
-//	}
+func TestPort2AlibabaCreateConnection_DIGP(t *testing.T) {
 
-//func TestPort2AzureCreateConnection_DIGP(t *testing.T) {
-//
-//	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-//		TerraformDir: "../../examples/port-2-azure-connection",
-//	})
-//
-//	defer terraform.Destroy(t, terraformOptions)
-//	t.Parallel()
-//
-//	terraform.InitAndApply(t, terraformOptions)
-//	output := terraform.Output(t, terraformOptions, "azure_connection_id")
-//	assert.NotNil(t, output)
-//}
-//
-//func TestPort2AwsCreateConnection_DIGP(t *testing.T) {
-//
-//	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-//		TerraformDir: "../../tests/examples-without-external-providers/port-2-aws-connection",
-//	})
-//
-//	defer terraform.Destroy(t, terraformOptions)
-//	t.Parallel()
-//
-//	terraform.InitAndApply(t, terraformOptions)
-//	output := terraform.Output(t, terraformOptions, "aws_connection_id")
-//	assert.NotNil(t, output)
-//}
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../../examples/port-2-alibaba-connection",
+	})
+
+	defer terraform.Destroy(t, terraformOptions)
+	t.Parallel()
+
+	terraform.InitAndApply(t, terraformOptions)
+	output := terraform.Output(t, terraformOptions, "alibaba_connection_id")
+	assert.NotNil(t, output)
+}
+
+func TestPort2AwsCreateConnection_DIGP(t *testing.T) {
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../../tests/examples-without-external-providers/port-2-aws-connection",
+	})
+
+	defer terraform.Destroy(t, terraformOptions)
+	t.Parallel()
+
+	terraform.InitAndApply(t, terraformOptions)
+	output := terraform.Output(t, terraformOptions, "aws_connection_id")
+	assert.NotNil(t, output)
+}
+
+func TestPort2AzureCreateConnection_DIGP(t *testing.T) {
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../../tests/examples-without-external-providers/port-2-azure-connection",
+	})
+
+	defer terraform.Destroy(t, terraformOptions)
+	t.Parallel()
+
+	terraform.InitAndApply(t, terraformOptions)
+	output := terraform.Output(t, terraformOptions, "azure_connection_id")
+	assert.NotNil(t, output)
+	terraform.Apply(t, terraformOptions)
+
+	terraformOptions = terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		Vars: map[string]interface{}{
+			"connection_name": "P2Azure_Name_Update",
+		},
+		TerraformDir: "../../tests/examples-without-external-providers/port-2-azure-connection",
+	})
+	terraform.Apply(t, terraformOptions)
+}
 
 func TestPort2GoogleCreateConnection_DIGP(t *testing.T) {
 
@@ -72,19 +81,19 @@ func TestPort2GoogleCreateConnection_DIGP(t *testing.T) {
 	terraform.Apply(t, terraformOptions)
 }
 
-// func TestPort2Ibm2CreateConnection_DIGP(t *testing.T) {
-//
-//		terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-//			TerraformDir: "../../examples/port-2-ibm2-connection",
-//		})
-//
-//		defer terraform.Destroy(t, terraformOptions)
-//		t.Parallel()
-//
-//		terraform.InitAndApply(t, terraformOptions)
-//		output := terraform.Output(t, terraformOptions, "ibm2_connection_id")
-//		assert.NotNil(t, output)
-//	}
+func TestPort2Ibm2CreateConnection_DIGP(t *testing.T) {
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../../examples/port-2-ibm2-connection",
+	})
+
+	defer terraform.Destroy(t, terraformOptions)
+	t.Parallel()
+
+	terraform.InitAndApply(t, terraformOptions)
+	output := terraform.Output(t, terraformOptions, "ibm2_connection_id")
+	assert.NotNil(t, output)
+}
 
 func TestPort2PortCreateConnection_DIGP(t *testing.T) {
 
@@ -111,19 +120,19 @@ func TestPort2PortCreateConnection_DIGP(t *testing.T) {
 	terraform.Apply(t, terraformOptions)
 }
 
-// func TestPort2PrivateServiceProfileCreateConnection_DIGP(t *testing.T) {
-//
-//		terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-//			TerraformDir: "../../examples/port-2-private-service-profile-connection",
-//		})
-//
-//		defer terraform.Destroy(t, terraformOptions)
-//		t.Parallel()
-//
-//		terraform.InitAndApply(t, terraformOptions)
-//		output := terraform.Output(t, terraformOptions, "private_sp_connection_id")
-//		assert.NotNil(t, output)
-//	}
+func TestPort2PrivateServiceProfileCreateConnection_DIGP(t *testing.T) {
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../../examples/port-2-private-service-profile-connection",
+	})
+
+	defer terraform.Destroy(t, terraformOptions)
+	t.Parallel()
+
+	terraform.InitAndApply(t, terraformOptions)
+	output := terraform.Output(t, terraformOptions, "private_sp_connection_id")
+	assert.NotNil(t, output)
+}
 
 func TestCloudRouterCreate_DIGP(t *testing.T) {
 
@@ -149,81 +158,74 @@ func TestCloudRouterCreate_DIGP(t *testing.T) {
 	terraform.Apply(t, terraformOptions)
 }
 
-// func TestCloudRouter2AwsCreateConnection_DIGP(t *testing.T) {
-//
-//		terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-//			TerraformDir: "../../tests/examples-without-external-providers/cloud-router-2-aws-connection",
-//		})
-//
-//		defer terraform.Destroy(t, terraformOptions)
-//		t.Parallel()
-//
-//		terraform.InitAndApply(t, terraformOptions)
-//		output := terraform.Output(t, terraformOptions, "aws_connection_id")
-//		assert.NotNil(t, output)
-//	}
-//func TestCloudRouter2AzureCreateConnection_DIGP(t *testing.T) {
-//
-//	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-//		TerraformDir: "../../tests/examples-without-external-providers/cloud-router-2-azure-connection",
-//	})
-//
-//	defer terraform.Destroy(t, terraformOptions)
-//	t.Parallel()
-//
-//	terraform.InitAndApply(t, terraformOptions)
-//	output := terraform.Output(t, terraformOptions, "azure_connection_id")
-//	assert.NotNil(t, output)
-//
-//	terraform.Apply(t, terraformOptions)
-//
-//	terraformOptions = terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-//		Vars: map[string]interface{}{
-//			"connection_name": "FCR2Azure_Name_Update",
-//		},
-//		TerraformDir: "../../tests/examples-without-external-providers/cloud-router-2-azure-connection",
-//	})
-//	terraform.Apply(t, terraformOptions)
-//}
+func TestCloudRouter2AwsCreateConnection_DIGP(t *testing.T) {
 
-//func TestCloudRouter2PortRoutingProtocolCreateConnection_DIGP(t *testing.T) {
-//
-//	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-//		TerraformDir: "../../tests/examples-without-external-providers/cloud-router-2-port-routing-protocol-connection",
-//	})
-//
-//	defer terraform.Destroy(t, terraformOptions)
-//	t.Parallel()
-//
-//	terraform.InitAndApply(t, terraformOptions)
-//	output := terraform.Output(t, terraformOptions, "port_connection_id")
-//	assert.NotNil(t, output)
-//
-//	terraform.Apply(t, terraformOptions)
-//
-//	terraformOptions = terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-//		Vars: map[string]interface{}{
-//			"connection_name": "FCR2Port_Name_Update",
-//			"bandwidth":       50,
-//		},
-//		TerraformDir: "../../tests/examples-without-external-providers/cloud-router-2-port-routing-protocol-connection",
-//	})
-//	terraform.Apply(t, terraformOptions)
-//}
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../../tests/examples-without-external-providers/cloud-router-2-aws-connection",
+	})
 
-// func TestCloudRouter2ServiceProfileCreateConnection_DIGP(t *testing.T) {
-//
-//		terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-//			TerraformDir: "../../examples/cloud-router-2-service-profile-connection",
-//		})
-//
-//		defer terraform.Destroy(t, terraformOptions)
-//		t.Parallel()
-//
-//		terraform.InitAndApply(t, terraformOptions)
-//		output := terraform.Output(t, terraformOptions, "service_profile_connection_id")
-//		assert.NotNil(t, output)
-//	}
+	defer terraform.Destroy(t, terraformOptions)
+	t.Parallel()
+
+	terraform.InitAndApply(t, terraformOptions)
+	output := terraform.Output(t, terraformOptions, "aws_connection_id")
+	assert.NotNil(t, output)
+}
+
+func TestCloudRouter2AzureCreateConnection_DIGP(t *testing.T) {
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../../tests/examples-without-external-providers/cloud-router-2-azure-connection",
+	})
+
+	defer terraform.Destroy(t, terraformOptions)
+	t.Parallel()
+
+	terraform.InitAndApply(t, terraformOptions)
+	output := terraform.Output(t, terraformOptions, "azure_connection_id")
+	assert.NotNil(t, output)
+}
+
+func TestCloudRouter2PortRoutingProtocolCreateConnection_DIGP(t *testing.T) {
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../../tests/examples-without-external-providers/cloud-router-2-port-routing-protocol-connection",
+	})
+
+	defer terraform.Destroy(t, terraformOptions)
+	t.Parallel()
+
+	terraform.InitAndApply(t, terraformOptions)
+	output := terraform.Output(t, terraformOptions, "port_connection_id")
+	assert.NotNil(t, output)
+
+	terraform.Apply(t, terraformOptions)
+
+	terraformOptions = terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		Vars: map[string]interface{}{
+			"connection_name": "FCR2P_Update",
+			"bandwidth":       50,
+			"bgp_rp_name":     "BGP_RP_Update",
+			"direct_rp_name":  "Direct_RP_Update",
+		},
+		TerraformDir: "../../tests/examples-without-external-providers/cloud-router-2-port-routing-protocol-connection",
+	})
+	terraform.Apply(t, terraformOptions)
+}
+
+func TestCloudRouter2ServiceProfileCreateConnection_DIGP(t *testing.T) {
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../../examples/cloud-router-2-service-profile-connection",
+	})
+
+	defer terraform.Destroy(t, terraformOptions)
+	t.Parallel()
+
+	terraform.InitAndApply(t, terraformOptions)
+	output := terraform.Output(t, terraformOptions, "service_profile_connection_id")
+	assert.NotNil(t, output)
+}
 
 func TestCloudRouter2WanCreateConnection_DIGP(t *testing.T) {
 
@@ -274,42 +276,33 @@ func TestPort2WanCreateConnection_DIGP(t *testing.T) {
 	terraform.Apply(t, terraformOptions)
 }
 
-// func TestVirtualDevice2WanCreateConnection_DIGP(t *testing.T) {
-//
-//		terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-//			TerraformDir: "../../tests/examples-without-external-providers/virtual-device-2-wan-connection",
-//		})
-//
-//		defer terraform.Destroy(t, terraformOptions)
-//		t.Parallel()
-//
-//		terraform.InitAndApply(t, terraformOptions)
-//		output := terraform.Output(t, terraformOptions, "wan_connection_id")
-//		assert.NotNil(t, output)
-//	}
-//func TestVirtualDevice2AzureCreateConnection_DIGP(t *testing.T) {
-//
-//	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-//		TerraformDir: "../../tests/examples-without-external-providers/virtual-device-2-azure-connection",
-//	})
-//
-//	defer terraform.Destroy(t, terraformOptions)
-//	t.Parallel()
-//
-//	terraform.InitAndApply(t, terraformOptions)
-//	output := terraform.Output(t, terraformOptions, "azure_connection_id")
-//	assert.NotNil(t, output)
-//
-//	terraform.Apply(t, terraformOptions)
-//
-//	terraformOptions = terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-//		Vars: map[string]interface{}{
-//			"connection_name": "VD2Azure_Name_Update",
-//		},
-//		TerraformDir: "../../tests/examples-without-external-providers/virtual-device-2-azure-connection",
-//	})
-//	terraform.Apply(t, terraformOptions)
-//}
+func TestVirtualDevice2WanCreateConnection_DIGP(t *testing.T) {
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../../tests/examples-without-external-providers/virtual-device-2-wan-connection",
+	})
+
+	defer terraform.Destroy(t, terraformOptions)
+	t.Parallel()
+
+	terraform.InitAndApply(t, terraformOptions)
+	output := terraform.Output(t, terraformOptions, "wan_connection_id")
+	assert.NotNil(t, output)
+}
+
+func TestVirtualDevice2AzureCreateConnection_DIGP(t *testing.T) {
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../../tests/examples-without-external-providers/virtual-device-2-azure-connection",
+	})
+
+	defer terraform.Destroy(t, terraformOptions)
+	t.Parallel()
+
+	terraform.InitAndApply(t, terraformOptions)
+	output := terraform.Output(t, terraformOptions, "azure_connection_id")
+	assert.NotNil(t, output)
+}
 
 func TestVirtualDevice2PortCreateConnection_DIGP(t *testing.T) {
 
