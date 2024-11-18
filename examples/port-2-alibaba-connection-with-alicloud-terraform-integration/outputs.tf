efc72cb00739bba96d83d38814447576702215f5
d-bhola <dbhola@equinix.com>
2024-11-18 14:28:03 -0800

    CXF-99867: Integrate Alibaba Terraform Provider with Fabric Connection Examples (#155)
    * CXF-99867: Port to Alibaba Example
    
    * CXF-99867: Fix special characters error and minor improvements
    
    * CXF-99867: Fix typo
    
    * CXF-99867: Address PR comments
    
    * CXF-99867: Update README.md
    
    * CXF-99867: Update step 2 in README.md
    
    * CXF-99867: Address PR comments
    
    * CXF-99867: Address PR comments
    
    * CXF-99867: Address PR comments
    
    * CXF-99867: Add bullets to the deletion step

---
+++ examples/port-2-alibaba-connection-with-alicloud-terraform-integration/outputs.tf
@@ -0,0 +1,21 @@
+output "alibaba_connection" {
+  value = module.create_port_2_alibaba_connection.primary_connection
+  sensitive = true
+}
+
+output "alibaba_connection_id" {
+  value = module.create_port_2_alibaba_connection.primary_connection_id
+}
+
+# output "alicloud_express_connect_virtual_border_router" {
+#   value = alicloud_express_connect_virtual_border_router.vbr
+#   sensitive = true
+# }
+#
+# output "alicloud_express_connect_virtual_border_router_id" {
+#   value = alicloud_express_connect_virtual_border_router.vbr.id
+# }
+#
+# output "express_connect_virtual_border_router_id" {
+#   value = data.alicloud_express_connect_virtual_border_routers.nameRegexVbr.routers.0.id
+# }
