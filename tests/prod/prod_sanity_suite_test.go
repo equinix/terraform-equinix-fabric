81437add9443e06b4272537cc605fb48223b21da
d-bhola <dbhola@equinix.com>
2025-02-13 09:55:04 -0800

    CXF-104025: Fix typo

--- tests/prod/prod_sanity_suite_test.go
+++ tests/prod/prod_sanity_suite_test.go
@@ -45,7 +45,7 @@ func TestPort2AwsCreateConnection_DIGP(t *testing.T) {
 func TestPort2AzureCreateConnection_DIGP(t *testing.T) {
 
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		TerraformDir: "../../example/port-2-azure-connection",
+		TerraformDir: "../../examples/port-2-azure-connection",
 	})
 
 	defer terraform.Destroy(t, terraformOptions)
