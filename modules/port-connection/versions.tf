ebdafb761a29a145996a32e6e4750af9e21875bc
d-bhola <dbhola@equinix.com>
2025-07-25 11:14:46 -0700

    CXF-113767: Change module version

--- modules/port-connection/versions.tf
+++ modules/port-connection/versions.tf
@@ -1,9 +1,8 @@
 terraform {
-  required_version = ">= 1.5.4"
   required_providers {
     equinix = {
-      source  = "equinix/equinix"
-      version = ">= 3.2.0"
+      source = "developer.equinix.com/terraform/equinix"
+      version = "9.0.0"
     }
   }
 }
