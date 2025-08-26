7fdf152c44c04e8a37c66b693daa2f950ab52235
d-bhola <dbhola@equinix.com>
2025-08-25 21:58:11 -0700

    CXF-110987: Add port-2-etree-network example

---
+++ examples/port-2-etree-network/outputs.tf
@@ -0,0 +1,8 @@
+output "port_connection" {
+  value = module.create_port_2_network_connection.primary_connection
+  sensitive = true
+}
+
+output "port_connection_id" {
+  value = module.create_port_2_network_connection.primary_connection_id
+}
