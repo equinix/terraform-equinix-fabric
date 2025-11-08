a4d5295f5e1f1cd99fd5ebea4ddbaa37dcfa7d68
d-bhola <dbhola@equinix.com>
2025-11-08 15:09:50 -0800

    Remove comments

--- modules/port-connection/versions.tf
+++ modules/port-connection/versions.tf
@@ -2,10 +2,8 @@ terraform {
   required_version = ">= 1.5.4"
   required_providers {
     equinix = {
-      # source  = "equinix/equinix"
-      # version = ">= 3.2.0"
-      source = "developer.equinix.com/terraform/equinix"
-      version = "9.0.0"
+      source  = "equinix/equinix"
+      version = ">= 4.9.0"
     }
   }
 }
