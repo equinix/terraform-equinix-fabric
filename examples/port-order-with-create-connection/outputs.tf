332e56210775453f10a43e44d874645823875081
d-bhola <dbhola@equinix.com>
2025-11-08 07:25:26 -0800

    Update with new filters param

--- examples/port-order-with-create-connection/outputs.tf
+++ examples/port-order-with-create-connection/outputs.tf
@@ -1,14 +1,14 @@
-output "port_uuid" {
-  value = equinix_fabric_port.order.id
-}
-
-output "port_name" {
-  value       = equinix_fabric_port.order.name
-}
-
-output "port_state" {
-  value = data.equinix_fabric_port.check_active.state
-}
+# output "port_uuid" {
+#   value = equinix_fabric_port.order.id
+# }
+#
+# output "port_name" {
+#   value       = equinix_fabric_port.order.name
+# }
+#
+# output "port_state" {
+#   value = data.equinix_fabric_port.check_active.state
+# }
 
 output "public_sp_connection_id" {
   value = module.create_port_2_public_sp_connection.primary_connection_id
---
+++ examples/port-ordering-port-2-public-service-profile-connection/versions.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.5.4"
+  required_providers {
+    equinix = {
+      source = "developer.equinix.com/terraform/equinix"
+      version = "9.0.0"
+      # source  = "equinix/equinix"
+      # version = ">= 3.2.0"
+    }
+  }
+}
+#
+# terraform {
+#   required_providers {
+#     equinix = {
+#       source = "developer.equinix.com/terraform/equinix"
+#       version = "9.0.0"
+#     }
+#   }
+# }
