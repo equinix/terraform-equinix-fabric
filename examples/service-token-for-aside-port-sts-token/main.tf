21e0f0fdbfe8045997ee6745b915cf66819aa9ca
Bhoomi Shah <bhshah@equinix.com>
2026-01-13 11:56:41 -0800

    fix review comments

--- examples/service-token-for-aside-port-sts-token/main.tf
+++ examples/service-token-for-aside-port-sts-token/main.tf
@@ -1,6 +1,6 @@
 provider "equinix" {
-  sts_source_token = var.sts_source_token
-  sts_auth_scope = var.sts_auth_scope
+  token_exchange_subject_token = var.sts_source_token
+  token_exchange_scope = var.sts_auth_scope
 }
 data "equinix_fabric_ports" "aside_port" {
   filters {
