f907568dc50296c6991a4143047d139af5f9f7f1
Bhoomi Shah <bhshah@equinix.com>
2026-01-15 09:39:25 -0800

    STS token changes for FCR router, route filters and route protocols

--- examples/cloud-router-2-service-profile-connection-sts-token/main.tf
+++ examples/cloud-router-2-service-profile-connection-sts-token/main.tf
@@ -3,6 +3,30 @@ provider "equinix" {
   token_exchange_scope = var.sts_auth_scope
 }
 
+resource "equinix_fabric_cloud_router" "this" {
+  type = var.cloud_router_type
+  name = var.cloud_router_name
+  location {
+    metro_code = var.cloud_router_metro_code
+  }
+  package {
+    code = var.cloud_router_package
+  }
+  order {
+    purchase_order_number = var.purchase_order_number
+  }
+  notifications {
+    type   = var.notifications_type
+    emails = var.notifications_emails
+  }
+  project {
+    project_id = var.project_id
+  }
+  account {
+    account_number = var.account_number
+  }
+}
+
 module "cloud_router_sp_connection" {
   source = "../../modules/cloud-router-connection"
 
@@ -14,11 +38,34 @@ module "cloud_router_sp_connection" {
   purchase_order_number = var.purchase_order_number
 
   #Aside
-  aside_fcr_uuid = var.aside_fcr_uuid
+  aside_fcr_uuid = equinix_fabric_cloud_router.this.id
 
   #Zside
   zside_ap_type         = var.zside_ap_type
   zside_ap_profile_type = var.zside_ap_profile_type
   zside_location        = var.zside_location
   zside_fabric_sp_name  = var.zside_fabric_sp_name
+}
+
+module "cloud_router_connection_routing_protocols" {
+  source = "../../modules/cloud-router-routing-protocols"
+  connection_uuid = module.cloud_router_sp_connection.primary_connection_id
+  direct_rp_name = var.routing_protocol_direct_rp_name
+  direct_equinix_ipv4_ip = var.routing_protocol_direct_ipv4_ip
+  bgp_rp_name = var.routing_protocol_bgp_rp_name
+  bgp_customer_peer_ipv4 = var.routing_protocol_bgp_ipv4_ip
+  bgp_customer_asn = var.routing_protocol_bgp_customer_asn
+}
+
+module "cloud_router_route_filters" {
+  depends_on = [ module.cloud_router_connection_routing_protocols ]
+  source                            = "../../modules/cloud-router-route-filters"
+  connection_id                     = module.cloud_router_sp_connection.primary_connection_id
+  connection_route_filter_direction = var.route_filter_direction
+
+  route_filter_policy_name       = var.route_filter_policy_name
+  route_filter_policy_project_id = var.project_id
+  route_filter_policy_type       = var.route_filter_policy_type
+
+  route_filter_rules = var.route_filter_rules
 }
