c1fa965ea7723c7f3511975196d8bf58981ac4f9
Bhoomi Shah <bhshah@equinix.com>
2026-01-15 16:41:46 -0800

    STS token changes for FCR router, route filters and route protocols (#238)

--- examples/cloud-router-2-service-profile-connection-sts-token/variables.tf
+++ examples/cloud-router-2-service-profile-connection-sts-token/variables.tf
@@ -26,10 +26,6 @@ variable "purchase_order_number" {
   type        = string
   default     = ""
 }
-variable "aside_fcr_uuid" {
-  description = "Equinix-assigned Fabric Cloud Router identifier"
-  type        = string
-}
 variable "zside_ap_type" {
   description = "Access point type - COLO, VD, VG, SP, IGW, SUBNET, GW"
   type        = string
@@ -59,4 +55,76 @@ variable "sts_auth_scope" {
   description = "Equinix STS Source Token Authentication Scope in the format: roleassignments:{rootOrg}"
   type        = string
   sensitive   = true
+}
+variable "cloud_router_name" {
+  description = "Name of the cloud router created for the connection"
+  type        = string
+}
+
+variable "cloud_router_type" {
+  description = "Type of the cloud router created for the connection"
+  type        = string
+}
+
+variable "cloud_router_metro_code" {
+  description = "Name of the cloud router created for the connection"
+  type        = string
+}
+
+variable "cloud_router_package" {
+  description = "Package of the cloud router created for the connection"
+  type        = string
+}
+
+variable "project_id" {
+  description = "Id of the Fabric Project for the resources to be created in"
+  type        = string
+}
+
+variable "account_number" {
+  description = "Account number for the cloud router creation"
+  type        = number
+}
+variable "routing_protocol_direct_rp_name" {
+  description = "Name of the Direct Routing Protocol Added to the Cloud Router Connection"
+  type = string
+}
+variable "routing_protocol_direct_ipv4_ip" {
+  description = "Ipv4 Ip address for Cloud Router Direct Routing Protocol"
+  type = string
+}
+variable "routing_protocol_bgp_rp_name" {
+  description = "Ipv4 Ip address for Cloud Router Bgp Routing Protocol"
+}
+variable "routing_protocol_bgp_ipv4_ip" {
+  description = "Ipv4 Ip address for Cloud Router Direct Routing Protocol"
+  type = string
+}
+variable "routing_protocol_bgp_customer_asn" {
+  description = "Customer ASN number for BGP Routing Protocol"
+  type = number
+}
+variable "route_filter_direction" {
+  description = "Direction of the route filtering [INBOUND or OUTBOUND]"
+  type        = string
+}
+
+variable "route_filter_policy_name" {
+  description = "Name of the route filter policy that will be created in this module"
+  type        = string
+}
+
+variable "route_filter_policy_type" {
+  description = "Type of the route filter policy. Should be one of: BGP_IPv4_PREFIX_FILTER, BGP_IPv6_PREFIX_FILTER"
+  type        = string
+}
+
+variable "route_filter_rules" {
+  description = "List of route filter rules to add to the created route filter policy"
+  type = list(object({
+    prefix       = string,
+    name         = optional(string),
+    description  = optional(string),
+    prefix_match = optional(string),
+  }))
 }
