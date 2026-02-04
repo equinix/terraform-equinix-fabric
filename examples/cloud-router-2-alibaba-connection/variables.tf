1960e7fc98b0c8a889e191ba616f7a2840dd88e7
Bhoomi Shah <bhshah@equinix.com>
2026-02-04 15:55:42 -0800

    Add FCR to Alibaba connection example

---
+++ examples/cloud-router-2-alibaba-connection/variables.tf
@@ -0,0 +1,84 @@
+variable "equinix_client_id" {
+  description = "Equinix client ID (consumer key), obtained after registering app in the developer platform"
+  type        = string
+  sensitive   = true
+}
+variable "equinix_client_secret" {
+  description = "Equinix client secret ID (consumer secret), obtained after registering app in the developer platform"
+  type        = string
+  sensitive   = true
+}
+variable "connection_name" {
+  description = "Connection name. An alpha-numeric 24 characters string which can include only hyphens and underscores"
+  type        = string
+}
+variable "connection_type" {
+  description = "Defines the connection type like VG_VC, EVPL_VC, EPL_VC, EC_VC, IP_VC, ACCESS_EPL_VC"
+  type        = string
+}
+variable "notifications_type" {
+  description = "Notification Type - ALL is the only type currently supported"
+  type        = string
+  default     = "ALL"
+}
+variable "notifications_emails" {
+  description = "Array of contact emails"
+  type        = list(string)
+}
+variable "bandwidth" {
+  description = "Connection bandwidth in Mbps"
+  type        = number
+}
+variable "purchase_order_number" {
+  description = "Purchase order number"
+  type        = string
+  default     = ""
+}
+variable "aside_fcr_uuid" {
+  description = "Equinix-assigned Fabric Cloud Router identifier"
+  type        = string
+}
+variable "zside_ap_type" {
+  description = "Access point type - COLO, VD, VG, SP, IGW, SUBNET, GW"
+  type        = string
+}
+variable "zside_ap_authentication_key" {
+  description = "Authentication key for provider based connections"
+  type        = string
+  sensitive   = true
+}
+variable "zside_ap_profile_type" {
+  description = "Service profile type - L2_PROFILE, L3_PROFILE, ECIA_PROFILE, ECMC_PROFILE"
+  type        = string
+}
+variable "zside_location" {
+  description = "Access point metro code"
+  type        = string
+}
+variable "zside_sp_name" {
+  description = "Equinix Service Profile Name"
+  type        = string
+}
+variable "zside_seller_region" {
+  description = "Access point seller region"
+  type        = string
+}
+variable "routing_protocol_direct_rp_name" {
+  description = "Name of the Direct Routing Protocol Added to the Cloud Router Connection"
+  type        = string
+}
+variable "routing_protocol_direct_ipv4_ip" {
+  description = "Ipv4 Ip address for Cloud Router Direct Routing Protocol"
+  type        = string
+}
+variable "routing_protocol_bgp_rp_name" {
+  description = "Ipv4 Ip address for Cloud Router Bgp Routing Protocol"
+}
+variable "routing_protocol_bgp_ipv4_ip" {
+  description = "Ipv4 Ip address for Cloud Router Direct Routing Protocol"
+  type        = string
+}
+variable "routing_protocol_bgp_customer_asn" {
+  description = "Customer ASN number for BGP Routing Protocol"
+  type        = number
+}
