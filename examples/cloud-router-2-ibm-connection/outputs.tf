4175f089b234e431e663a9b356bbb95edaad585f
Bhoomi Shah <bhshah@equinix.com>
2026-02-05 09:20:06 -0800

    feat: Add FCR to IBM connection

---
+++ examples/cloud-router-2-ibm-connection/outputs.tf
@@ -0,0 +1,11 @@
+output "ibm2_connection" {
+  value     = module.create_fcr_2_ibm2_connection.primary_connection
+  sensitive = true
+}
+output "IBM_Gateway_Action_Id" {
+  value = ibm_dl_gateway_action.test_dl_gateway_action.id
+}
+
+output "ibm2_connection_id" {
+  value = module.create_fcr_2_ibm2_connection.primary_connection_id
+}
